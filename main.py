0ff6d547bb1e80e30327fe92c46b720a1b1f8d64
Dwayne Burrows <burrows.dwayne@icloud.com>
2024-10-31 01:13:52 +0100

    basic gui window setup

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import tkinter as tk
+
+root = tk.Tk()
+
+root.title("Post Master General")
+root.geometry("600x400+50+50")
+# adjust gui transparency
+# root.attributes("-alpha", 0.5)
+
+root.mainloop()
